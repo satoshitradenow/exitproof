d4c632aab6a2d31b613bc462a0fa0a6a5de1cd86
satoshitradenow <satoshitradenow@gmail.com>
2025-11-05 15:17:08 -0500

    Restore original website homepage exactly

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 export default function Page() {
-  const filePath = path.join(process.cwd(), 'public', 'site.html');
-  const html = fs.readFileSync(filePath, 'utf8');
+  const filePath = path.join(process.cwd(), "public", "site.html");
+  const html = fs.readFileSync(filePath, "utf8");
   return (
     <div dangerouslySetInnerHTML={{ __html: html }} />
   );
